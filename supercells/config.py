9bf97a8f04f8bf493e59159263f5f02f9e73f26f
Roy Granit, PhD <rgranit@users.noreply.github.com>
2024-06-09 15:29:48 +0300

    Update config.py
    Change staturation to 30%

--- supercells/config.py
+++ supercells/config.py
@@ -5,7 +5,7 @@ CUTOFFS_DICT: dict[str, float] = {
     "Median Genes per Cell": 1_500,
     "Valid Barcodes": 75,
     # "Valid UMIs": 75, # TODO: replace with valid metric found in CellRanger output summary
-    "Sequencing Saturation": 20000,
+    "Sequencing Saturation": 30,
     "Q30 Bases in RNA Read": 65,
     "Fraction Reads in Cells": 70,
     "Reads Mapped to Genome": 75,
